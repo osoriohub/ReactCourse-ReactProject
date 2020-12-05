8a2495741b1a010cb281f870e92871d3c8f57373
osoriohub <68162885+osoriohub@users.noreply.github.com>
2020-12-05 08:31:26 -0600

    Primera Versión completa
    5 de Diciembre 2020 Omar Osorio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AppClass from './components/AppClass'
+import AppFunction from './components/AppFunction'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <AppClass />
+      <AppFunction />
+    </>
+  )
+}
+
+export default App
